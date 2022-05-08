e0ab6a17e5823fec1ca9a4646979c358371485d3
PraneelChugh <86180715+PraneelChugh@users.noreply.github.com>
2022-05-08 12:49:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference=0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -12,9 +16,21 @@ function setup(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX)
     }
 }
 
 function modelLoaded(){
     console.log("PoseNet is Initialised !");
+}
+
+function draw(){
+    background("#969A97");
+
+    textSize(difference);
+    fill('#F90093');
+   text('Peter',50,400)
 }
